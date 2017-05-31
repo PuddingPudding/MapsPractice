94780423a147c30f346805fae69cc207fd9b8000
PuddingPudding <hwsh70119@gmail.com>
2017-05-31 13:39:00 +0800

    成功做出弓在移動時會前後搖擺的動畫效果

---
+++ Assets/BowUser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowUser : MonoBehaviour
+{
+
+    public Animator bowAnimator;
+    private Rigidbody rigidbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+           this.bowAnimator.SetFloat("speed", this.rigidbody.velocity.sqrMagnitude);
+
+
+    }
+}
