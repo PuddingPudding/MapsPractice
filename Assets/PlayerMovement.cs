69dbb4bf5c2b6dca2dec69c97f71c87e5e92c29a
PuddingPudding <hwsh70119@gmail.com>
2017-07-05 15:08:43 +0800

    幹，commit訊息打到另一邊，讓Observer只能走在路上和跳躍，但做到一半

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float rotateSpeed = 1.5f;
+    public float currentSpeed;
+    public float moveSpeed;
+    public Rigidbody rigidBody;
+    public float currentRotateX = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //決定鍵盤input的結果
+        Vector3 movDirection = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) { movDirection.z += 1; }
+        if (Input.GetKey(KeyCode.S)) { movDirection.z -= 1; }
+        if (Input.GetKey(KeyCode.D)) { movDirection.x += 1; }
+        if (Input.GetKey(KeyCode.A)) { movDirection.x -= 1; }
+        movDirection = movDirection.normalized;
+
+        rigidBody.velocity = movDirection * moveSpeed;
+
+        this.transform.localEulerAngles += new Vector3(0, Input.GetAxis("Horizontal"), 0) * rotateSpeed;
+        Vector3 finalRotation = this.transform.localEulerAngles;
+        currentRotateX -= Input.GetAxis("Vertical") * rotateSpeed; ;
+
+        if (currentRotateX > 90)
+        {
+            //this.transform.eulerAngles = new Vector3(90, 0, 0);
+            currentRotateX = 90;
+        }
+        if (currentRotateX < -90)
+        {
+            //this.transform.localEulerAngles.Set(-90, this.transform.localEulerAngles.y, this.transform.localEulerAngles.z);
+            currentRotateX = -90;
+        }
+        finalRotation.x = currentRotateX;
+        this.transform.localEulerAngles = finalRotation;
+    }
+}
