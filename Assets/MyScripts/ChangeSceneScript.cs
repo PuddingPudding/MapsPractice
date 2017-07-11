392462dfc1b4e80097e7f4ebaea7cef83b0d3def
PuddingPudding <hwsh70119@gmail.com>
2017-07-11 12:49:14 +0800

    目前已經有主選單跳轉至教學關卡的程式碼，實裝了上去，還未有敵人

---
+++ Assets/MyScripts/ChangeSceneScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSceneScript : MonoBehaviour
+{
+
+    public void ChangeToScene(string SceneName)
+    {
+        Application.LoadLevel(SceneName);
+    }
+
+    public void ChangeToScene(int SceneID)
+    {
+        Application.LoadLevel(SceneID);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
